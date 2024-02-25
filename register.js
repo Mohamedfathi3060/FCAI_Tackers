f82d88582f54c459f1006306374e4011936daa7c
Mohamed Fathi <Mohamedfathi11119@gmail.com>
2024-02-25 21:04:36 +0200

    touch N25

--- register.js
+++ register.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const player = require("play-sound")();
 
 const accountSid = "AC05868868a9948e692ad790ebe91b94e1";
-const authToken = "518e1286a3c85a69e1d5342d7a8da885";
+const authToken = "e2af53a15928c8680532601a734fc84b";
 const client = require("twilio")(accountSid, authToken);
 
 // Send Request to check if register
@@ -14,10 +14,13 @@ const headers = {
     "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMDIxMDM0OCIsImF1dGgiOiJST0xFX1NUVURFTlQiLCJleHAiOjE3MTAzNjc2ODN9.dxJGLPG-GpZwML1ccwxpALkxkparlGW2FkCoDY25O2rW45mzggoX2goIRQ_Jn0E1FEhjTV9lWqNvCXZ5IUiRlQ",
   // Add more headers if needed
 };
-const sendMessageTo = (number) => {
+const appear = `Registration is opened Now, Hurry! ⌛⌛⌛`;
+const NotAppear = `Testing ...`;
+
+const sendMessageTo = (number, text) => {
   client.messages
     .create({
-      body: `Registration is opened Now, Hurry! ⌛⌛⌛`,
+      body: text,
       from: "whatsapp:+14155238886",
       to: `whatsapp:+2${number}`,
     })
@@ -33,17 +36,16 @@ const checkRegistration = async () => {
     res.data.prerequistesSatistfiedCourses.length != 0 ||
     res.data.responseCode != -1
   ) {
-    sendMessageTo("01113931845");
-    sendMessageTo("01024068783");
-    sendMessageTo("01149800106");
-    sendMessageTo("01093598773");
-    sendMessageTo("01224781985");
+    sendMessageTo("01024068783", appear);
+    sendMessageTo("01113931845", appear);
+    sendMessageTo("01149800106", appear);
+    sendMessageTo("01093598773", appear);
+    sendMessageTo("01224781985", appear);
   } else {
     // Still Not Opened
     console.log("Still not Openeed");
   }
 };
 checkRegistration();
-sendMessageTo("01113931845");
-sendMessageTo("01024068783");
-setInterval(checkRegistration, 300000);
+sendMessageTo("01024068783", NotAppear);
+setInterval(checkRegistration, 30000);
