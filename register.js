ea2c7e8844e679a5a45e3ac8aa613c244bb777bb
Mohamed Fathi <Mohamedfathi11119@gmail.com>
2024-10-12 15:00:27 +0300

    adding push Over notifications

--- register.js
+++ register.js
@@ -1,49 +1,42 @@
 const axios = require("axios");
 require("dotenv").config();
-
-const client = require("twilio")(
-  process.env.ACCOUNT_SID,
-  process.env.AUTH_TOKEN
-);
-
-const headers = {
-  "Content-Type": "application/json",
-  Authorization: "Bearer " + process.env.TOKEN, // Add more headers if needed
-};
+const Pushover = require("pushover-notifications");
+const p = new Pushover({
+  user: process.env.PUSH_OVER_HONORx10,
+  token: process.env.PUSH_OVER_TOKEN,
+});
 const appear = `Registration is opened Now, Hurry! ⌛⌛⌛`;
 const NotAppear = `Testing ...`;
 
-const sendMessageTo = (number, text) => {
-  client.messages
-    .create({
-      body: text,
-      from: "whatsapp:+14155238886",
-      to: `whatsapp:+2${number}`,
-    })
-    .then((message) => console.log(message.sid));
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: "Bearer " + process.env.FCAI_TOKEN, // Add more headers if needed
 };
+
+const sendMessageTo = (message) => {
+  p.send({ message }, (err, result) => {
+    if (err) console.error(err);
+    else console.log("Message Sent  =>  " + result);
+  });
+};
+
 const checkRegistration = async () => {
   const res = await axios({
     method: "GET",
-    url: "http://193.227.14.58/api/student-registration-courses?studentId=20210348",
+    url: process.env.FCAI_URL,
     headers,
   });
   if (
     res.data.prerequistesSatistfiedCourses.length != 0 ||
-    res.data.responseCode != -1
+    res.data.responseCode != -1 ||
+    res.data.registeredCourses.length != 0
   ) {
-    sendMessageTo("01024068783", appear);
-    sendMessageTo("01113931845", appear);
-    sendMessageTo("01149800106", appear);
-    sendMessageTo("01093598773", appear);
-    sendMessageTo("01224781985", appear);
-    sendMessageTo("01155313475", appear);
+    sendMessageTo(appear);
   } else {
     // Still Not Opened
     console.log(res.data);
     console.log("Still not Openeed");
   }
 };
-checkRegistration();
-sendMessageTo("01024068783", NotAppear);
-setInterval(checkRegistration, 60000);
+sendMessageTo(NotAppear);
+setInterval(checkRegistration, 10 * 1000);
